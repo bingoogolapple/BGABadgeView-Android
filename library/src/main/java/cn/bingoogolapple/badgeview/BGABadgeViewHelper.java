81c93c554416f4b24a595ba8c9f7811f3de8b814
王浩 <bingoogolapple@gmail.com>
2015-07-16 20:05:35 +0800

    fix #3

--- library/src/main/java/cn/bingoogolapple/badgeview/BGABadgeViewHelper.java
+++ library/src/main/java/cn/bingoogolapple/badgeview/BGABadgeViewHelper.java
@@ -280,8 +280,8 @@ public class BGABadgeViewHelper {
         // 计算徽章背景的宽高
         int badgeHeight = mBadgeNumberRect.height() + mBadgePadding * 2;
         int badgeWidth;
-        // 当mBadgeText的长度为1时，计算出来的高度会比宽度大，此时设置宽度等于高度
-        if (badgeText.length() == 1) {
+        // 当mBadgeText的长度为1或0时，计算出来的高度会比宽度大，此时设置宽度等于高度
+        if (badgeText.length() == 1 || badgeText.length() == 0) {
             badgeWidth = badgeHeight;
         } else {
             badgeWidth = mBadgeNumberRect.width() + mBadgePadding * 2;
