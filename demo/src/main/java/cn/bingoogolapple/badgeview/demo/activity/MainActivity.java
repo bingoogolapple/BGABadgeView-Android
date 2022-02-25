c0a49141b81a9ecc95b8cc880ebe3b2cfc61f200
“chenrun” <runc@rings.net.cn>
2022-02-25 10:41:26 +0800

    可编译

--- demo/src/main/java/cn/bingoogolapple/badgeview/demo/activity/MainActivity.java
+++ demo/src/main/java/cn/bingoogolapple/badgeview/demo/activity/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToastUtil.init(this);
+       // ToastUtil.init(this);
 
         initView();
         testBadgeView();
